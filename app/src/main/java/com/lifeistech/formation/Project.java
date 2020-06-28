0e9c132737c7cca1f4db9a76fdb044f23a81fa94
uraihajime <hajime.u223@ezweb.ne.jp>
2020-06-28 16:17:17 +0900

    dancersクラス

---
+++ app/src/main/java/com/lifeistech/formation/Project.java
@@ -0,0 +1,13 @@
+package com.lifeistech.formation;
+
+import java.util.List;
+
+public class Project {
+    List<Dancer> dancers;
+    String name;
+
+    Project(List<Dancer> dancers,String name){
+        this.dancers = dancers;
+        this.name = name;
+}
+}
